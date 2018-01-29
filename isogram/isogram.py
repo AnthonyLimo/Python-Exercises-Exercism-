a469359781b3f55429fe1d918525f5becac03e60
Anthony Kiplimo <anthonylimo90@gmail.com>
2018-01-29 20:22:18 +0300

    Isogram challenge
    Basically checks if letters are repeated in a particular string

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,8 @@
 def is_isogram(string):
-    pass
+    lower_string = string.lower()
+    for char in lower_string:
+        if lower_string.count(char) > 1:
+            if char == '-' or char == ' ':
+                continue
+            return False
+    return True 
